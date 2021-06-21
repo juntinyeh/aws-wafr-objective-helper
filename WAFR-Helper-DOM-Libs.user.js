f0bf728847d1eafb93331f2dd2a8f4f4da78655f
juntinyeh <juntin.yeh@gmail.com>
2021-06-21 21:53:14 +0800

    append new function to handle append text/child reset into DOM libs

--- WAFR-Helper-DOM-Libs.user.js
+++ WAFR-Helper-DOM-Libs.user.js
@@ -15,3 +15,30 @@ function div_reset_innerHTML(id)
     div.innerHTML = '';
 }
 
+function div_append_text(id, text)
+{
+    var div = document.getElementById(id);
+    div.innerHTML += text;
+}
+
+function div_append_child(id, ele)
+{
+    var div = document.getElementsById(id);
+    div.appendChild(ele);
+}
+
+function div_format_key_value_to_text(key, value)
+{
+    return '<h2>' + key + '</h2>' + '<p>' + value + '</p><hr />';
+}
+
+function div_format_value_list_to_text(value)
+{
+
+    var value_text = '';
+    JSON_value.forEach(append_to_text);
+    function append_to_text(item, index){
+        value_text += " - " + item + "<br />";
+    }
+    return value_text;
+}
