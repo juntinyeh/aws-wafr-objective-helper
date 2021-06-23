16a23867ba0e85e65038ddfcfc00ee80076d5d99
juntinyeh <juntin.yeh@gmail.com>
2021-06-23 16:41:20 +0800

    fix inline spacing

--- WAFR-Context-Helper.user.js
+++ WAFR-Context-Helper.user.js
@@ -95,15 +95,16 @@ var oh_div_context_helper_header = document.createElement('button');
     oh_div_context_helper_header.id = 'oh_div_context_helper_header';
     oh_div_context_helper_header.className = "awsui-button awsui-button-variant-primary";
     oh_div_context_helper_header.innerHTML = 'Context ▼';
-    oh_div_context_helper_header.style = 'text-align:center; width:160px';
-    oh_div_context_helper_header.style.width = '160px';
-    oh_div_context_helper_header.addEventListener("click", function() {        
+    oh_div_context_helper_header.style = 'text-align:center; width:160px; display: inline';
+//    oh_div_context_helper_header.style.width = '160px';
+    oh_div_context_helper_header.addEventListener("click", function() {
         div_ani_click_toggle('oh_div_context_helper_header','oh_div_context_helper_container', 'Context ');
-        DOM_Context_Helper_Refresh_Check();
+        //DOM_Context_Helper_Refresh_Check();
     });
 
 var oh_div_context_helper_language = document.createElement('select');
     oh_div_context_helper_language.id = 'oh_div_context_helper_language';
+oh_div_context_helper_language.style.display = 'inline';
 
     for (const [key, value] of Object.entries(JSON_SUPPORTED_LANG))
         {
@@ -127,7 +128,10 @@ var oh_div_context_helper_reload = document.createElement('a');
     })
 
     oh_div_context_helper.appendChild(oh_div_context_helper_header);
-    oh_div_context_helper.innerHTML += '&nbsp;';
+var div = document.createElement('div');
+    div.innerHTML = '&nbsp;';
+    div.style.display = 'inline';
+    oh_div_context_helper.appendChild(div);
     oh_div_context_helper.appendChild(oh_div_context_helper_language);
     //oh_div_context_helper.appendChild(oh_div_context_helper_reload);
     oh_div_context_helper.appendChild(oh_div_context_helper_container);
